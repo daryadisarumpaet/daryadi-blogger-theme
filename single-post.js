6d617275926eb2fd54503e6bff09596d38e97fc1
daryadisarumpaet <142488393+daryadisarumpaet@users.noreply.github.com>
2025-08-18 07:44:07 -0600

    Update single-post.js

--- single-post.js
+++ single-post.js
@@ -1,1 +1,4 @@
+const replybox = document.querySelector('#top-ce'); // Or, .comment-replybox-thread
+const comment_form = document.querySelector('.comment-form');
 
+comment_form.appendChild(replybox);
